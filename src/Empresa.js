8f7f166bb2584275529941c6230e972e667bcf8e
Bogdan Alin <alinugr@gmail.com>
2015-10-20 20:32:01 +0200

    subir fuentes

---
+++ src/Empresa.js
@@ -0,0 +1,22 @@
+//Empresa.js
+
+var Empresa = function(codigo,nombre,calificacion){
+
+	this.codigo;
+	this.nombre;
+	this.calificacion;
+	
+	this.printInfo=function(){
+		console.log('el codigo de la empresa es:', codigo);
+		console.log('el nombre de la empresa es:', nombre);
+		console.log('el calificacion de la empresa es:', calificacion);
+	}
+
+	this.test=function(){
+		console.log('testing');
+	}
+
+	return this;
+
+}
+module.exports.Empresa = Empresa;
---
+++ src/main.js
@@ -0,0 +1,16 @@
+//main.js
+
+var empresa = require('./Empresa.js');
+
+var coleccionEmpresas = [];
+
+for(var i=0; i<4;i++){
+	var esta_empresa=new empresa.Empresa('x',0,10);
+	coleccionEmpresas.push(esta_empresa);
+}
+
+coleccionEmpresas[0].calificacion=9;
+
+for( var i in coleccionEmpresas){
+	coleccionEmpresas[i].printInfo();
+}
